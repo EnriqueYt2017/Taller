1f2eb0fa66d62478d80be9f26f8e00334ea5ef59
franlobosm <fran.lobosm@duocuc.cl>
2024-07-04 05:24:51 -0400

    Intento de bajar el peso del proyecto

---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: [
+        './dashboard/templates/dashboard/**/*.html',
+        './dashboard/static/dashboard/static/**/*.js'
+    ],
+    css: ['./dashboard/static/dashboard/static/**/*.css'],
+    output: './static/css/purged'
+}
